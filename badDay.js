936b4d9f24d6dd38e3079a50a22c531b12a68d1c
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-19 17:25:17 -0400

    it's still not working

---
+++ badDay.js
@@ -0,0 +1,12 @@
+//step one:
+const day = require('./day.js');
+
+//step two:
+const badDay = function(day){
+   for(let i = 0; i < day.length; i++){
+       console.log('hi')
+   } 
+}
+
+//step three:
+setInterval(badDay,1000)
